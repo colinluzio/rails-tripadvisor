d008ce574a604b50758221c801a25bb99085e384
Colin Luzio <colinluzio@yahoo.co.uk>
2015-07-27 07:27:49 +0100

    add additional author information

---
+++ db/migrate/20150726182803_drop_author_table.rb
@@ -0,0 +1,9 @@
+class DropAuthorTable < ActiveRecord::Migration
+  def up
+    drop_table :authors
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
---
+++ db/migrate/20150726183554_create_authors.rb
@@ -0,0 +1,11 @@
+class CreateAuthors < ActiveRecord::Migration
+  def change
+    create_table :authors do |t|
+	  t.string :name
+	  t.string :age
+	  t.string :country
+	  
+      t.timestamps
+    end
+  end
+end
