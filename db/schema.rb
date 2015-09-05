ca26041b3f74f0e2f806a52888f365e22b1ee7c6
Colin Luzio <colinluzio@yahoo.co.uk>
2015-09-05 14:55:30 +0100

    show number of stars

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150831062845) do
+ActiveRecord::Schema.define(version: 20150905132423) do
 
   create_table "authors", force: true do |t|
     t.string   "name"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20150831062845) do
     t.integer  "author_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "stars"
   end
 
   create_table "countries", force: true do |t|
