43bc30894406404d6a865900baa6bf2df6e99c21
Colin Luzio <colinluzio@yahoo.co.uk>
2015-10-13 08:07:00 +0100

    check is existing booking for user on same day

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,4 +2,21 @@ class Booking < ActiveRecord::Base
 	has_one :hotel
 	has_one :user
 	before_save { |booking| booking.status = 0 }
+	validate :is_information_valid
+	
+	private
+	def is_information_valid
+
+	# Get all user bookings at same hotel
+	     
+		@bookings = Booking.where(['user_id = ? and hotel_id = ?', self.user_id, self.hotel_id])
+		
+		@bookings.each do | i |
+			if i.startdate == self.startdate
+				 errors.add(:comments, "Not valid start date")
+			end
+			
+		end
+		
+	end
 end
